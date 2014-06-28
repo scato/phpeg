c208cc996ae96d710c5238c1d9ca780de4786a63
Scato Eggen <scato.eggen@gmail.com>
2014-06-28 22:56:34 +0200

    Compile Choice

--- Literal.php
+++ Literal.php
@@ -13,9 +13,7 @@ class Literal implements Parser, Compiler
 
     public function parse(Input $input)
     {
-        $restStr = $input->restStr();
-
-        if (substr($restStr, 0, $this->len) === $this->str) {
+        if (substr($input->restStr(), 0, $this->len) === $this->str) {
             $input->skip($this->len);
 
             return new Success($this->str);
@@ -30,15 +28,13 @@ class Literal implements Parser, Compiler
         $len = var_export($this->len, true);
 
         return '
-        $restStr = $input->restStr();
+            if (substr($input->restStr(), 0, ' . $len . ') === ' . $str . ') {
+                $input->skip(' . $len . ');
 
-        if (substr($restStr, 0, ' . $len . ') === ' . $str . ') {
-            $input->skip(' . $len . ');
-
-            $result = new Success(' . $str . ');
-        } else {
-            $result = new Failure("Expected: \'" . ' . $str . ' . "\' at " . $input->at());
-        }
+                $result = new Success(' . $str . ');
+            } else {
+                $result = new Failure("Expected: \'" . ' . $str . ' . "\' at " . $input->at());
+            }
         ';
     }
 }
