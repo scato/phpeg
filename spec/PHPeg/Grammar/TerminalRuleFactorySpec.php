47c6eac4d1fefa83c5851fb7e69da36be3edd6ae
Scato Eggen <seggen@ibuildings.nl>
2014-07-22 22:25:11 +0200

    Added factory for terminal rules

---
+++ spec/PHPeg/Grammar/TerminalRuleFactorySpec.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace spec\PHPeg\Grammar;
+
+use PHPeg\Combinator\Context;
+use PHPeg\ExpressionInterface;
+use PHPeg\Grammar\Tree\AnyNode;
+use PHPeg\Grammar\Tree\CharacterClassNode;
+use PHPeg\Grammar\Tree\LiteralNode;
+use PHPeg\Grammar\Tree\RuleReferenceNode;
+use PHPeg\GrammarInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TerminalRuleFactorySpec extends ObjectBehavior
+{
+    function it_should_create_a_whitespace_rule()
+    {
+        $context = new Context();
+
+        $this->createWhitespace()->parse(' ', $context)->isSuccess()->shouldBe(true);
+        $this->createWhitespace()->parse(' ', $context)->getResult()->shouldBe(null);
+        $this->createWhitespace()->parse(' ', $context)->getRest()->shouldBe('');
+
+        $this->createWhitespace()->parse('foo', $context)->isSuccess()->shouldBe(true);
+        $this->createWhitespace()->parse('foo', $context)->getResult()->shouldBe(null);
+        $this->createWhitespace()->parse('foo', $context)->getRest()->shouldBe('foo');
+
+        $this->createWhitespace()->parse("\r\n\t", $context)->isSuccess()->shouldBe(true);
+        $this->createWhitespace()->parse("\r\n\t", $context)->getResult()->shouldBe(null);
+        $this->createWhitespace()->parse("\r\n\t", $context)->getRest()->shouldBe('');
+    }
+
+    function it_should_create_a_literal_rule()
+    {
+        $context = new Context();
+
+        $this->createLiteral()->parse('"foo"', $context)->isSuccess()->shouldBe(true);
+        $this->createLiteral()->parse('"foo"', $context)->getResult()->shouldBeLike(new LiteralNode('foo'));
+        $this->createLiteral()->parse('"foo"', $context)->getRest()->shouldBe('');
+
+        $this->createLiteral()->parse('foo', $context)->isSuccess()->shouldBe(false);
+
+        $this->createLiteral()->parse('"\\""', $context)->getResult()->shouldBeLike(new LiteralNode('"'));
+    }
+
+    function it_should_create_an_any_rule()
+    {
+        $context = new Context();
+
+        $this->createAny()->parse('.', $context)->isSuccess()->shouldBe(true);
+        $this->createAny()->parse('.', $context)->getResult()->shouldBeLike(new AnyNode());
+        $this->createAny()->parse('.', $context)->getRest()->shouldBeLike('');
+
+        $this->createAny()->parse('foo', $context)->isSuccess()->shouldBe(false);
+    }
+
+    function it_should_create_a_character_class_rule()
+    {
+        $context = new Context();
+
+        $this->createCharacterClass()->parse('[a-z]', $context)->isSuccess()->shouldBe(true);
+        $this->createCharacterClass()->parse('[a-z]', $context)->getResult()->shouldBeLike(new CharacterClassNode('a-z'));
+        $this->createCharacterClass()->parse('[a-z]', $context)->getRest()->shouldBeLike('');
+
+        $this->createCharacterClass()->parse('foo', $context)->isSuccess()->shouldBe(false);
+
+        $this->createCharacterClass()->parse('[^\\]]', $context)->getResult()->shouldBeLike(new CharacterClassNode('^\\]'));
+    }
+
+    function it_should_create_a_rule_reference_rule()
+    {
+        $context = new Context();
+
+        $this->createRuleReference()->parse('foo', $context)->isSuccess()->shouldBe(true);
+        $this->createRuleReference()->parse('foo', $context)->getResult()->shouldBeLike(new RuleReferenceNode('foo'));
+        $this->createRuleReference()->parse('foo', $context)->getRest()->shouldBe('');
+
+        $this->createRuleReference()->parse('1foo', $context)->isSuccess()->shouldBe(false);
+        $this->createRuleReference()->parse('_foo1', $context)->isSuccess()->shouldBe(true);
+    }
+}
