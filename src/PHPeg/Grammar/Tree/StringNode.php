8a08fe0b38017bafc877771998a755d4545308e1
Scato Eggen <seggen@ibuildings.nl>
2014-07-24 17:45:13 +0200

    Refactored some node classes

---
+++ src/PHPeg/Grammar/Tree/StringNode.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace PHPeg\Grammar\Tree;
+
+
+abstract class StringNode
+{
+    private $string;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public function getString()
+    {
+        return $this->string;
+    }
+}
