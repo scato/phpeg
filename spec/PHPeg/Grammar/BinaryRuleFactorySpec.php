1930ff4102a704de0c850fcd05f1e2f9b3cad7ec
Scato Eggen <scato.eggen@gmail.com>
2014-07-26 23:55:34 +0200

    Added binary operators

---
+++ spec/PHPeg/Grammar/BinaryRuleFactorySpec.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace spec\PHPeg\Grammar;
+
+use PHPeg\Combinator\Context;
+use PHPeg\Grammar\TerminalRuleFactory;
+use PHPeg\Grammar\Tree\ActionNode;
+use PHPeg\Grammar\Tree\ChoiceNode;
+use PHPeg\Grammar\Tree\LabelNode;
+use PHPeg\Grammar\Tree\RuleReferenceNode;
+use PHPeg\Grammar\Tree\SequenceNode;
+use PHPeg\GrammarInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BinaryRuleFactorySpec extends ObjectBehavior
+{
+    function let(GrammarInterface $grammar)
+    {
+        $terminalRuleFactory = new TerminalRuleFactory();
+
+        $grammar->getRule('_')->willReturn($terminalRuleFactory->createWhitespace());
+        $grammar->getRule('Predicate')->willReturn($terminalRuleFactory->createRuleReference());
+
+        $grammar->getRule('Label')->willReturn($this->createLabel($grammar));
+        $grammar->getRule('Sequence')->willReturn($this->createSequence($grammar));
+        $grammar->getRule('Code')->willReturn($this->createCode($grammar));
+        $grammar->getRule('Action')->willReturn($this->createAction($grammar));
+        $grammar->getRule('Choice')->willReturn($this->createChoice($grammar));
+    }
+
+    function it_should_create_a_label_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $this->createLabel($grammar)->parse('name:foo', $context)->isSuccess()->shouldBe(true);
+        $this->createLabel($grammar)->parse('name:foo', $context)->getResult()->shouldBeLike(new LabelNode('name', new RuleReferenceNode('foo')));
+        $this->createLabel($grammar)->parse('name:foo', $context)->getRest()->shouldBe('');
+    }
+
+    function it_should_create_a_sequence_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $this->createSequence($grammar)->parse('foo bar', $context)->isSuccess()->shouldBe(true);
+        $this->createSequence($grammar)->parse('foo bar', $context)->getResult()->shouldBeLike(new SequenceNode(new RuleReferenceNode('foo'), new RuleReferenceNode('bar')));
+        $this->createSequence($grammar)->parse('foo bar', $context)->getRest()->shouldBe('');
+
+        $this->createSequence($grammar)->parse('foo the bar', $context)->isSuccess()->shouldBe(true);
+        $this->createSequence($grammar)->parse('foo the bar', $context)->getResult()->shouldBeLike(new SequenceNode(new SequenceNode(new RuleReferenceNode('foo'), new RuleReferenceNode('the')), new RuleReferenceNode('bar')));
+        $this->createSequence($grammar)->parse('foo the bar', $context)->getRest()->shouldBe('');
+    }
+
+    function it_should_create_a_code_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $code = 'if (true) { return 1; } else { return 2; }';
+
+        $this->createCode($grammar)->parse($code, $context)->isSuccess()->shouldBe(true);
+        $this->createCode($grammar)->parse($code, $context)->getResult()->shouldBe($code);
+        $this->createCode($grammar)->parse($code, $context)->getRest()->shouldBe('');
+
+        $this->createCode($grammar)->parse($code . '}', $context)->isSuccess()->shouldBe(true);
+        $this->createCode($grammar)->parse($code . '}', $context)->getResult()->shouldBe($code);
+        $this->createCode($grammar)->parse($code . '}', $context)->getRest()->shouldBe('}');
+    }
+
+    function it_should_create_an_action_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $this->createAction($grammar)->parse('foo { return "bar"; }', $context)->isSuccess()->shouldBe(true);
+        $this->createAction($grammar)->parse('foo { return "bar"; }', $context)->getResult()->shouldBeLike(new ActionNode(new RuleReferenceNode('foo'), 'return "bar";'));
+        $this->createAction($grammar)->parse('foo { return "bar"; }', $context)->getRest()->shouldBe('');
+    }
+
+    function it_should_create_a_choice_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $this->createChoice($grammar)->parse('foo / bar', $context)->isSuccess()->shouldBe(true);
+        $this->createChoice($grammar)->parse('foo / bar', $context)->getResult()->shouldBeLike(new ChoiceNode(new RuleReferenceNode('foo'), new RuleReferenceNode('bar')));
+        $this->createChoice($grammar)->parse('foo / bar', $context)->getRest()->shouldBe('');
+
+        $this->createChoice($grammar)->parse('foo / the / bar', $context)->isSuccess()->shouldBe(true);
+        $this->createChoice($grammar)->parse('foo / the / bar', $context)->getResult()->shouldBeLike(new ChoiceNode(new ChoiceNode(new RuleReferenceNode('foo'), new RuleReferenceNode('the')), new RuleReferenceNode('bar')));
+        $this->createChoice($grammar)->parse('foo / the / bar', $context)->getRest()->shouldBe('');
+    }
+
+    function it_should_create_an_expression_rule(GrammarInterface $grammar)
+    {
+        $context = new Context();
+
+        $this->createExpression($grammar)->parse('left:foo right:bar / bar { return "bar"; }', $context)->isSuccess()->shouldBe(true);
+        $this->createExpression($grammar)->parse('left:foo right:bar / bar { return "bar"; }', $context)->getRest()->shouldBe('');
+    }
+}
---
+++ src/PHPeg/Grammar/BinaryRuleFactory.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace PHPeg\Grammar;
+
+use PHPeg\Combinator\Action;
+use PHPeg\Combinator\CharacterClass;
+use PHPeg\Combinator\Choice;
+use PHPeg\Combinator\Label;
+use PHPeg\Combinator\Literal;
+use PHPeg\Combinator\RuleReference;
+use PHPeg\Combinator\Sequence;
+use PHPeg\Combinator\ZeroOrMore;
+use PHPeg\GrammarInterface;
+
+class BinaryRuleFactory
+{
+    public function createLabel(GrammarInterface $grammar)
+    {
+        // Label = name:([A-Za-z_] [A-Za-z0-9_]*) _ ":" _ expression:Predicate { new LabelNode($name, $expression); } / Predicate;
+        return new Choice(
+            new Action(
+                new Sequence(
+                    new Sequence(
+                        new Sequence(
+                            new Sequence(
+                                new Label(
+                                    'name',
+                                    new Sequence(new CharacterClass('A-Za-z_'), new ZeroOrMore(new CharacterClass('A-Za-z0-9_')))
+                                ),
+                                new RuleReference($grammar, '_')
+                            ),
+                            new Literal(':')
+                        ),
+                        new RuleReference($grammar, '_')
+                    ),
+                    new Label('expression', new RuleReference($grammar, 'Predicate'))
+                ),
+                'return new \PHPeg\Grammar\Tree\LabelNode($name, $expression);'
+            ),
+            new RuleReference($grammar, 'Predicate')
+        );
+    }
+
+    public function createSequence(GrammarInterface $grammar)
+    {
+        // Sequence = left:Label (left:(_ right:Label { new SequenceNode($left, $right); }))* { return $left; };
+        return new Action(
+            new Sequence(
+                new Label('left', new RuleReference($grammar, 'Label')),
+                new ZeroOrMore(
+                    new Label(
+                        'left',
+                        new Action(
+                            new Sequence(
+                                new RuleReference($grammar, '_'),
+                                new Label('right', new RuleReference($grammar, 'Label'))
+                            ),
+                            'return new \PHPeg\Grammar\Tree\SequenceNode($left, $right);'
+                        )
+                    )
+                )
+            ),
+            'return $left;'
+        );
+    }
+
+    public function createCode(GrammarInterface $grammar)
+    {
+        // Code = ([^{}] / "{" Code "}")*;
+        return new ZeroOrMore(new Choice(
+            new CharacterClass('^{}'),
+            new Sequence(new Sequence(new Literal('{'), new RuleReference($grammar, 'Code')), new Literal('}'))
+        ));
+    }
+
+    public function createAction(GrammarInterface $grammar)
+    {
+        // Action = expression:Sequence _ "{" code:Code "}" { new ActionNode($expression, trim($code)); } / Sequence;
+        return new Choice(
+            new Action(
+                new Sequence(
+                    new Sequence(
+                        new Sequence(
+                            new Sequence(
+                                new Label('expression', new RuleReference($grammar, 'Sequence')),
+                                new RuleReference($grammar, '_')
+                            ),
+                            new Literal('{')
+                        ),
+                        new Label('code', new RuleReference($grammar, 'Code'))
+                    ),
+                    new Literal('}')
+                ),
+                'return new \PHPeg\Grammar\Tree\ActionNode($expression, trim($code));'
+            ),
+            new RuleReference($grammar, 'Sequence')
+        );
+    }
+
+    public function createChoice(GrammarInterface $grammar)
+    {
+        // Choice = left:Action (left:(_ "/" _ right:Action { new ChoiceNode($left, $right); }))* { return $left; };
+        return new Action(
+            new Sequence(
+                new Label('left', new RuleReference($grammar, 'Action')),
+                new ZeroOrMore(
+                    new Label(
+                        'left',
+                        new Action(
+                            new Sequence(
+                                new Sequence(
+                                    new Sequence(
+                                        new RuleReference($grammar, '_'),
+                                        new Literal("/")
+                                    ),
+                                    new RuleReference($grammar, '_')
+                                ),
+                                new Label('right', new RuleReference($grammar, 'Action'))
+                            ),
+                            'return new \PHPeg\Grammar\Tree\ChoiceNode($left, $right);'
+                        )
+                    )
+                )
+            ),
+            'return $left;'
+        );
+    }
+
+    public function createExpression(GrammarInterface $grammar)
+    {
+        // Expression = Choice;
+        return new RuleReference($grammar, 'Choice');
+    }
+}
