fe27e60c86298b6aacd65e9c49d326b2e3ad5b0d
Scato Eggen <scato.eggen@gmail.com>
2014-07-28 22:18:23 +0200

    We can now parse grammars with a parser

---
+++ spec/PHPeg/Grammar/ParserFactorySpec.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace spec\PHPeg\Grammar;
+
+use PHPeg\Grammar\BinaryRuleFactory;
+use PHPeg\Grammar\GrammarRuleFactory;
+use PHPeg\Grammar\TerminalRuleFactory;
+use PHPeg\Grammar\Tree\GrammarNode;
+use PHPeg\Grammar\Tree\RuleNode;
+use PHPeg\Grammar\Tree\RuleReferenceNode;
+use PHPeg\Grammar\UnaryRuleFactory;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ParserFactorySpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(
+            new TerminalRuleFactory(),
+            new UnaryRuleFactory(),
+            new BinaryRuleFactory(),
+            new GrammarRuleFactory()
+        );
+    }
+
+    function it_should_parse_a_grammar()
+    {
+        $definition = 'grammar foo { start foo = the; the = bar; }';
+
+        $tree = new GrammarNode('foo', 'foo', array(
+            new RuleNode('foo', new RuleReferenceNode('the')),
+            new RuleNode('the', new RuleReferenceNode('bar')),
+        ));
+
+        $this->createParser()->parse($definition)->shouldBeLike($tree);
+    }
+}
