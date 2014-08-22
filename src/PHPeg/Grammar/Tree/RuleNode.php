7fd684cc6d9258d49b3f74ef7a1e5a41086c984c
Scato Eggen <seggen@ibuildings.nl>
2014-08-22 08:19:34 +0200

    Added optional name parameter for rule nodes, defaults to the identifier

--- src/PHPeg/Grammar/Tree/RuleNode.php
+++ src/PHPeg/Grammar/Tree/RuleNode.php
@@ -4,6 +4,25 @@ namespace PHPeg\Grammar\Tree;
 
 class RuleNode extends AbstractNamedNode
 {
+    private $identifier;
+
+    public function __construct($identifier, $name, $expression = null)
+    {
+        if ($expression === null) {
+            $expression = $name;
+            $name = $identifier;
+        }
+
+        parent::__construct($name, $expression);
+
+        $this->identifier = $identifier;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
     public function accept(VisitorInterface $visitor)
     {
         parent::accept($visitor);
