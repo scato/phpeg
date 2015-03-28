7dd60c175e89c6207b8741e054ae82f4193f096e
Scato Eggen <seggen@ibuildings.nl>
2015-03-28 12:21:00 +0100

    Report missing end of file instead of unexpected string

--- src/PHPeg/Generator/ToClassVisitor.php
+++ src/PHPeg/Generator/ToClassVisitor.php
@@ -267,12 +267,14 @@ EOS;
 
         \$_success = \$this->parse{$node->getStartSymbol()}();
 
-        if (!\$_success) {
-            throw new \InvalidArgumentException("Syntax error, expecting {\$this->expecting()} on line {\$this->line()}");
+        if (\$this->position < strlen(\$this->string)) {
+            \$_success = false;
+
+            \$this->report(\$this->position, "end of file");
         }
 
-        if (\$this->position < strlen(\$this->string)) {
-            throw new \InvalidArgumentException("Syntax error, unexpected {\$this->rest()} on line {\$this->line()}");
+        if (!\$_success) {
+            throw new \InvalidArgumentException("Syntax error, expecting {\$this->expecting()} on line {\$this->line()}");
         }
 
         return \$this->value;
--- src/PHPeg/Grammar/PegFile.php
+++ src/PHPeg/Grammar/PegFile.php
@@ -3541,12 +3541,14 @@ class PegFile
 
         $_success = $this->parsePegFile();
 
-        if (!$_success) {
-            throw new \InvalidArgumentException("Syntax error, expecting {$this->expecting()} on line {$this->line()}");
+        if ($this->position < strlen($this->string)) {
+            $_success = false;
+
+            $this->report($this->position, "end of file");
         }
 
-        if ($this->position < strlen($this->string)) {
-            throw new \InvalidArgumentException("Syntax error, unexpected {$this->rest()} on line {$this->line()}");
+        if (!$_success) {
+            throw new \InvalidArgumentException("Syntax error, expecting {$this->expecting()} on line {$this->line()}");
         }
 
         return $this->value;
