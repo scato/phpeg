0b66c8adbb208bd4aa93a66e55d3e89e693b0bf3
Scato Eggen <scato.eggen@gmail.com>
2020-12-13 13:39:52 +0100

    Version bump

--- src/PHPeg/Console/Application.php
+++ src/PHPeg/Console/Application.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    const VERSION = '1.0.2';
+    const VERSION = '1.0.3';
 
     public function __construct()
     {
